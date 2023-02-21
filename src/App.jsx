7b8317716930843f9484aac7a4f11ec41a2ac8c2
Vikram0118 <vikramgit001@gmail.com>
2023-02-21 20:29:45 +0530

    changed the title stuffs

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import {Header, Body, Footer, NavBar} from './containers'
 import {Message, SideNavBar} from './components'
 
 const App = () => {
+
+  let docTitle = document.title;
+
+  window.addEventListener("blur" ,() => {
+    document.title ="Hire Me : ) 😄";
+  })
+  window.addEventListener("focus", () => {
+    document.title = docTitle;
+  })
+
   return (
     <div className='App'>
       <Message />
